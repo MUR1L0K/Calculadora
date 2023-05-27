7fd714df3e9501f8f9b98c44d539a1e9d97ed856
Murilo Klein <91572912+MUR1L0K@users.noreply.github.com>
2023-05-27 11:20:50 -0300

    update

---
+++ calculate.js
@@ -0,0 +1,67 @@
+const input = document.getElementById("input");
+const result = document.getElementById("result");
+const allowedKeys = [
+  "(",
+  ")",
+  "/",
+  "*",
+  "-",
+  "+",
+  "9",
+  "8",
+  "7",
+  "6",
+  "5",
+  "4",
+  "3",
+  "2",
+  "1",
+  "0",
+  ".",
+  "%",
+  " ",
+];
+
+input.focus();
+
+input.addEventListener("keydown", function (ev) {
+  ev.preventDefault();
+  if (allowedKeys.includes(ev.key)) {
+    input.value += ev.key;
+    return;
+  }
+  if (ev.key == "Backspace") {
+    input.value = input.value.slice(0, -1);
+  }
+  if (ev.key == "Enter") {
+    calculate();
+  }
+});
+
+document.querySelectorAll(".charKey").forEach(
+  ("click",
+  function (charKeyBtn) {
+    charKeyBtn.addEventListener("click", function () {
+      let valor = charKeyBtn.dataset.value;
+      input.value += valor;
+    });
+  })
+);
+
+document.getElementById("clear").addEventListener("click", function () {
+  input.value = "";
+  input.focus();
+  result.value = "";
+  result.classList.remove("error");
+});
+
+document.getElementById("equal").addEventListener("click", calculate);
+
+function calculate() {
+  result.value = "ERROR";
+  result.classList.add("error");
+  result.value = eval(input.value);
+  result.classList.remove("error");
+}
+
+export { calculate };
